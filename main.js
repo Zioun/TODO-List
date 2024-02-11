37a954410e0b69b1dc8325036090173844706982
Zioun <jionkhan0@gmail.com>
2024-02-12 00:53:01 +0600

    added

--- main.js
+++ main.js
@@ -2,23 +2,31 @@
 let alertClose = document.getElementById("alertClose").addEventListener('click',function(){
     document.getElementById("alertArea").style.display = "none";
 })
+let alertClose2 = document.getElementById("alertClose2").addEventListener('click',function(){
+    document.getElementById("alertArea2").style.display = "none";
+})
 
 // TODO submitBtn option
 let submitBtn = document.getElementById("submitBtn").addEventListener('click',function(){
-    let alertArea = document.getElementById("alertArea");
     let inputField = document.getElementById("inputField");
     let inputFieldValue = inputField.value;
     if(inputFieldValue == false){
+        let alertArea = document.getElementById("alertArea");
         alertArea.style.display ="block";
     }else{
-        let empty = document.getElementById("empty");
-        empty.style.display = "none";
-        let createElement = document.createElement("p");
-        createElement.style.borderBottom = "2px solid teal";
-        createElement.innerText = inputFieldValue;
-        let paragraphArea = document.getElementById("paragraphArea");
-        paragraphArea.appendChild(createElement);
-        console.log(paragraphArea)
-        inputField.value = " ";
+        if(inputFieldValue.length > 30){
+            let alertArea2 = document.getElementById("alertArea2");
+            alertArea2.style.display ="block";
+        }else{
+            let empty = document.getElementById("empty");
+            empty.style.display = "none";
+            let createElement = document.createElement("p");
+            createElement.style.borderBottom = "2px solid teal";
+            createElement.innerText = inputFieldValue;
+            let paragraphArea = document.getElementById("paragraphArea");
+            paragraphArea.appendChild(createElement);
+            console.log(paragraphArea)
+            inputField.value = " ";
+        }
     }
 })
