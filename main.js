dda5d2fee24e83623d8605306ff825dc68b3b336
Zioun <jionkhan0@gmail.com>
2024-02-11 22:13:46 +0600

    alert added

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
+// TODO alert option
 let alertClose = document.getElementById("alertClose").addEventListener('click',function(){
     document.getElementById("alertArea").style.display = "none";
+})
+
+// TODO submitBtn option
+let submitBtn = document.getElementById("submitBtn").addEventListener('click',function(){
+    let alertArea = document.getElementById("alertArea");
+    let inputField = document.getElementById("inputField");
+    let inputFieldValue = inputField.value;
+    if(inputFieldValue == false){
+        alertArea.style.display ="block";
+    }
 })
